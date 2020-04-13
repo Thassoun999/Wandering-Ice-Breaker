f03157205968ba937eb71d96277b2e7d43706dca
ako99 <ngoa9899@gmail.com>
2020-04-12 20:40:24 -0400

    Added Main Menu, Level select, and UI within levels. Options and Credits buttons yet to have functionality.

---
+++ Wandering Ice Breaker/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,59 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonFunctions : MonoBehaviour {
+
+    public string txt;
+    bool isInt;
+    int level = 0;
+
+    void Start()
+    {
+        txt = this.gameObject.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text;
+        isInt = int.TryParse(txt, out level);
+        if(level > PlayerPrefs.GetInt("levelAt"))
+        {
+            this.gameObject.GetComponent<Button>().interactable = false;
+        }
+    }
+
+    
+   public void selectScene()
+    {
+        if (isInt)
+        {
+            SceneManager.LoadScene(1 + level);
+        }
+        else if (txt == "back" || txt == "Main Menu")
+        {
+            SceneManager.LoadScene(0);
+        }
+        else if (txt == "play" || txt == "Play")
+        {
+            if(PlayerPrefs.GetInt("levelAt") + 1 > SceneManager.sceneCountInBuildSettings - 2)
+            {
+                SceneManager.LoadScene(PlayerPrefs.GetInt("levelAt"));
+            }
+            else
+            {
+                SceneManager.LoadScene(PlayerPrefs.GetInt("levelAt") + 1);
+            }
+        }
+        else if (txt == "levels" || txt == "Levels")
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (txt == "options" || txt == "Options")
+        {
+            SceneManager.LoadScene(0);
+        }
+        else if (txt == "credits" || txt == "Credits")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
--- Wandering Ice Breaker/Assets/Scripts/LevelManager.cs
+++ Wandering Ice Breaker/Assets/Scripts/LevelManager.cs
@@ -56,7 +56,7 @@ public class LevelManager : MonoBehaviour {
             
         }
 
-        if(centerR%2 != 0)
+        if(gridStatus.Count % 2 != 0)
         {
             centerR = gridStatus.Count / 2;
         }
@@ -64,7 +64,7 @@ public class LevelManager : MonoBehaviour {
         {
             centerR = (gridStatus.Count / 2) -0.5f;
         }
-        if (centerC % 2 != 0)
+        if (gridStatus[0].Count % 2 != 0)
         {
             centerC = gridStatus[0].Count / 2;
         }
@@ -223,7 +223,12 @@ public class LevelManager : MonoBehaviour {
         pm.pause = true;
         audioSource.PlayOneShot(winSound);
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextScene - 1 > PlayerPrefs.GetInt("levelAt"))
+        {
+            PlayerPrefs.SetInt("levelAt", nextScene - 1);
+        }
+        SceneManager.LoadScene(nextScene);
     }
 
 }
