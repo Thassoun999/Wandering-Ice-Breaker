8e81a7519c742f1842f670f25c47a47879819cf3
ako99 <ngoa9899@gmail.com>
2020-04-14 20:13:32 -0400

    Finished UI implementation

---
+++ Wandering Ice Breaker/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    public GameObject mainMenuButtons;
+    public GameObject settingsMenu;
+    public GameObject credits;
+    public Slider slider;
+
+
+    void Start()
+    {
+        //slider = GameObject.Find("/Canvas/settingsMenu/Slider").GetComponent<slider>();
+
+        if(mainMenuButtons != null && settingsMenu != null)
+        {
+            mainMenuButtons.SetActive(true);
+            settingsMenu.SetActive(false);
+        }
+
+        if (PlayerPrefs.HasKey("volume"))
+        {
+            slider.value = PlayerPrefs.GetFloat("volume");
+        }
+        else
+        {
+            slider.value = 1;
+            audioMixer.SetFloat("volume", Mathf.Log10(1) * 20);
+        }
+        
+    }
+
+    
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+
+    public void MainMenuTransitionBack()
+    {
+        mainMenuButtons.SetActive(true);
+        settingsMenu.SetActive(false);
+        credits.SetActive(false);
+    }
+
+    public void MainMenuTransitionSettings()
+    {
+        mainMenuButtons.SetActive(false);
+        settingsMenu.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void MainMenuTransitionCredits()
+    {
+        mainMenuButtons.SetActive(false);
+        settingsMenu.SetActive(false);
+        credits.SetActive(true);
+    }
+}
--- Wandering Ice Breaker/Assets/Scripts/WhiteFoxSpirit.cs
+++ Wandering Ice Breaker/Assets/Scripts/WhiteFoxSpirit.cs
@@ -14,44 +14,48 @@ public class WhiteFoxSpirit : Enemy
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown("up") || Input.GetKeyDown("w")))
+        if (pm.pause == false)
         {
-            if (walkableTiles.Contains(manager.gridStatus[row + 1][col]))
+            if ((Input.GetKeyDown("up") || Input.GetKeyDown("w")))
             {
-                gameObject.transform.position = gameObject.transform.position + new Vector3(0, -0.64f, 0);
-                row++;
-            }
-
-        }
-        if ((Input.GetKeyDown("down") || Input.GetKeyDown("s")))
-        {
-            if (walkableTiles.Contains(manager.gridStatus[row - 1][col]))
-            {
-                gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0.64f, 0);
-                row--;
-               
-            }
-
-        }
-        if ((Input.GetKeyDown("left") || Input.GetKeyDown("a")))
-        {
-            if (walkableTiles.Contains(manager.gridStatus[row][col + 1]))
-            {
-                gameObject.transform.position = gameObject.transform.position + new Vector3(0.64f, 0, 0);
-                col++;
-                
-            }
-
-        }
-        if ((Input.GetKeyDown("right") || Input.GetKeyDown("d")))
-        {
-            if (walkableTiles.Contains(manager.gridStatus[row][col - 1]))
-            {
-                gameObject.transform.position = gameObject.transform.position + new Vector3(-0.64f, 0, 0);
-                col--;
+                if (walkableTiles.Contains(manager.gridStatus[row + 1][col]))
+                {
+                    gameObject.transform.position = gameObject.transform.position + new Vector3(0, -0.64f, 0);
+                    row++;
+                }
 
             }
+            if ((Input.GetKeyDown("down") || Input.GetKeyDown("s")))
+            {
+                if (walkableTiles.Contains(manager.gridStatus[row - 1][col]))
+                {
+                    gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0.64f, 0);
+                    row--;
 
+                }
+
+            }
+            if ((Input.GetKeyDown("left") || Input.GetKeyDown("a")))
+            {
+                if (walkableTiles.Contains(manager.gridStatus[row][col + 1]))
+                {
+                    gameObject.transform.position = gameObject.transform.position + new Vector3(0.64f, 0, 0);
+                    col++;
+
+                }
+
+            }
+            if ((Input.GetKeyDown("right") || Input.GetKeyDown("d")))
+            {
+                if (walkableTiles.Contains(manager.gridStatus[row][col - 1]))
+                {
+                    gameObject.transform.position = gameObject.transform.position + new Vector3(-0.64f, 0, 0);
+                    col--;
+
+                }
+
+            }
         }
+        
     }
 }
